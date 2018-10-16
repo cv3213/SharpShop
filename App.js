c9d4fb2c6fbb6d1ccef85ef38e32edd6d45d7724
Shinpei <kmh011225@naver.com>
2018-10-16 12:40:48 +0900

    make SharpScreen

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import HomeScreen from './components/HomeScreen';
 import SignInScreen from './components/SignInScreen';
 import SignUpScreen from './components/SignUpScreen';
 import MainScreen from './components/MainScreen';
+import ProfileScreen from './components/ProfileScreen';
+import SharpScreen from './components/SharpScreen';
 
 const App = createStackNavigator({
   Home: { screen: HomeScreen },
   SignIn: { screen: SignInScreen },
   SignUp: { screen: SignUpScreen },
   Main: { screen: MainScreen },
+  Profile: { screen: ProfileScreen },
+  Sharp: { screen: SharpScreen },
 });
 
 export default App;
